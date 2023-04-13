85ef5afb434fafdc286aecae24d1f1941d8be738
quanber123 <quanber123@icloud.com>
2023-04-13 20:25:29 +0700

    commit4

--- src/components/layouts/blog/new.js
+++ src/components/layouts/blog/new.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LazyLoad from 'react-lazyload';
 import Spinner from '../../../Spinner';
 import { Link } from 'react-router-dom';
 import '../../../assets/css/blog.css';
 
 function New({ blogs }) {
-  const blog = blogs.map((blog, index) => {
+  const [countPage, setCountPage] = useState(1);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const blogsInPage = 6;
+  const endIndex = currentIndex + blogsInPage;
+  const blog = blogs.slice(currentIndex, endIndex).map((blog, index) => {
     return (
       <article className='blog p-4' key={index}>
         <div className='relative'>
@@ -23,6 +27,21 @@ function New({ blogs }) {
       </article>
     );
   });
+  function handlePrevClick() {
+    if (currentIndex === 0) return;
+    setCountPage((prevCount) => prevCount - 1);
+    const newIndex = currentIndex === 0 ? 0 : currentIndex - blogsInPage;
+    setCurrentIndex(newIndex);
+  }
+  function handleNextClick() {
+    if (currentIndex === blogs.length - 1) return;
+    setCountPage((prevCount) => prevCount + 1);
+    const newIndex =
+      currentIndex === blogs.length - blogsInPage
+        ? currentIndex
+        : currentIndex + blogsInPage;
+    setCurrentIndex(newIndex);
+  }
   return (
     <section className='left-active'>
       <div className='flex nav-blog items-center'>
@@ -33,9 +52,20 @@ function New({ blogs }) {
         <h1 className='text-xl text-pink'>News</h1>
       </div>
       <div className='m-auto flex flex-wrap p-10'>{blog}</div>
-      <div className='flex justify-center text-pink'>
-        <i className='far fa-arrow-alt-circle-left'>a</i>
-        <i className='far fa-arrow-alt-circle-right'>b</i>
+      <div className='flex justify-center items-center mb-8'>
+        <i
+          className={`fa fa-angle-left text-3xl mx-4 ${
+            currentIndex === 0 ? 'hidden' : ''
+          }`}
+          onClick={handlePrevClick}
+        ></i>
+        <h1 className='bg-cyan text-white text-xl count-page'>{countPage}</h1>
+        <i
+          className={`fa fa-angle-right text-3xl mx-4 ${
+            currentIndex >= blogs.length - 6 ? 'hidden' : ''
+          }`}
+          onClick={handleNextClick}
+        ></i>
       </div>
     </section>
   );
