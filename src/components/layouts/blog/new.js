11c0739ddac32e8c245f8fb81ded7b98ff12dcb7
quanber123 <quanber123@icloud.com>
2023-04-13 16:53:42 +0700

    commit2

--- src/components/layouts/blog/new.js
+++ src/components/layouts/blog/new.js
@@ -3,8 +3,11 @@ import LazyLoad from 'react-lazyload';
 import Spinner from '../../../Spinner';
 import { Link } from 'react-router-dom';
 import '../../../assets/css/blog.css';
+import { useState } from 'react';
 
 function New({ blogs }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [countPage, setCountPage] = useState(1);
   const blog = blogs.map((blog, index) => {
     return (
       <article className='blog p-4' key={index}>
@@ -23,19 +26,34 @@ function New({ blogs }) {
       </article>
     );
   });
+  function handlePrevClick() {
+    setCountPage((prevCount) => prevCount--);
+    const newIndex = currentIndex === 0 ? blogs.length - 6 : currentIndex - 6;
+    setCurrentIndex(newIndex);
+  }
+  function handleNextClick() {
+    setCountPage((prevCount) => prevCount++);
+    const newIndex = currentIndex === blogs.length - 6 ? 0 : currentIndex + 6;
+    setCurrentIndex(newIndex);
+  }
   return (
     <section className='left-active'>
       <div className='flex nav-blog items-center'>
-        <Link className='text-xl text-cyan ml-10' to='/'>
+        <Link className='text-xl text-cyan m-2 ml-16' to='/'>
           Home
         </Link>
-        <h1 className='text-xl text-cyan mx-4'>/</h1>
-        <h1 className='text-xl text-pink'>News</h1>
+        <h1 className='text-xl text-cyan m-2 '>/</h1>
+        <h1 className='text-xl text-pink  m-2 '>News</h1>
       </div>
       <div className='m-auto flex flex-wrap p-10'>{blog}</div>
       <div className='flex justify-center text-pink'>
-        <i className='far fa-arrow-alt-circle-left'>a</i>
-        <i className='far fa-arrow-alt-circle-right'>b</i>
+        <i className='far fa-arrow-alt-circle-left' onClick={handlePrevClick}>
+          a
+        </i>
+        <h1>{countPage}</h1>
+        <i className='far fa-arrow-alt-circle-right' onClick={handleNextClick}>
+          b
+        </i>
       </div>
     </section>
   );
