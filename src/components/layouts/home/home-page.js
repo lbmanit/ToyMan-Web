08bde27c1a6f39977a341ae48c29bd320ebe40aa
quanber123 <quanber123@icloud.com>
2023-05-02 01:46:07 +0700

    fix lazy

--- src/components/layouts/home/home-page.js
+++ src/components/layouts/home/home-page.js
@@ -1,21 +1,18 @@
-import React, { lazy } from 'react';
-import { Suspense } from 'react';
+import React from 'react';
 import Spinner from '../../../app/Spinner';
 import LazyLoad from 'react-lazyload';
-const Header = lazy(() => import('./header/header'));
-const Main = lazy(() => import('./main/main'));
+import Header from './header/header';
+import Main from './main/main';
 function Home() {
   return (
-    <Suspense fallback={<Spinner />}>
-      <React.Fragment>
-        <LazyLoad height={100} once placeholder={<Spinner />}>
-          <Header />
-        </LazyLoad>
-        <LazyLoad height={100} once placeholder={<Spinner />}>
-          <Main />
-        </LazyLoad>
-      </React.Fragment>
-    </Suspense>
+    <React.Fragment>
+      <LazyLoad height={100} once placeholder={<Spinner />}>
+        <Header />
+      </LazyLoad>
+      <LazyLoad height={100} once placeholder={<Spinner />}>
+        <Main />
+      </LazyLoad>
+    </React.Fragment>
   );
 }
 export default Home;
